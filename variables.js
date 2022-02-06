2b6500077ab14b5f6b72caf933dcb849e04ac722
Bastiche <bastien.velitchkine@live.fr>
2022-02-06 17:22:52 +0100

    [+] Global Variables

---
+++ variables.js
@@ -0,0 +1,21 @@
+function globalVariables() {
+    return Object.freeze({
+      "SPREADSHEET": {
+        "spreadsheetId": "1P2Hw9y4qwFwm1qDUKDncxzi8diiDtQGhv0Ft0zYeUv4",
+        "contactSheet": {
+          "sheetName": "Contacts",
+          "email": "A:A",
+          "phone": "B:B",
+          "status": "C:C",
+          "lastStatusUpdate": "D:D"
+        },
+        "parametersSheet": {
+          "sheetName": "Parameters",
+          "lastCheckDate": "A2"
+        }
+      },
+      "PROCESSING": {
+        "phoneStopList": ["0760769872"]
+      }
+    });
+  };
